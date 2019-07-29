547d15480bbfd8a943912b830ff857def2cdb0d4
chadrm9 <chad.r.milburn@gmail.com>
2019-07-28 20:03:09 -0400

    feed spotipy data to numpy first draft

---
+++ polka/helpers.py
@@ -0,0 +1,7 @@
+def Base64Decode(jsonDump):
+    loaded = json.loads(jsonDump)
+    dtype = np.dtype(loaded[0])
+    arr = np.frombuffer(base64.decodestring(loaded[1]),dtype)
+    if len(loaded) > 2:
+        return arr.reshape(loaded[2])
+    return arr
